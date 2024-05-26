89800632514e95b6b1e4ea0ca50d1391b447755f
luciD <sosalnet@yandex.ru>
2024-05-26 23:28:43 +0300

    update cors 2.0

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(
   cors({
     origin: '*',
     allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
   })
 );
 
